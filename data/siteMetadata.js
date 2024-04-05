ca4587605cd26e1c9b688d20f2aed6cc589ffc63
David Umoru <umorudavido@gmail.com>
2024-04-05 08:01:07 +0100

    Comment out unused Analytics configuration

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,9 +24,9 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    googleAnalytics: {
-      googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
-    },
+    // googleAnalytics: {
+    //  googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+    // },
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
