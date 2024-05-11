07fedccedce13eb4ec7dd0e5d75e105d0f2c7fd1
David Umoru <umorudavido@gmail.com>
2024-05-11 15:50:08 +0100

    Update project data and add new projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,9 +8,15 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'Scryer',
-    description: `Coming Soon`,
-    imgSrc: '',
-    href: '/',
+    description: `Scryer leverages the power of Gemini, a large language model from Google AI, combined with web scraping techniques to unlock valuable insights from web data.`,
+    imgSrc: '/static/images/scryer.png',
+    href: 'https://github.com/davidumoru/scryer',
+  },
+  {
+    title: 'EtherSeed',
+    description: `EtherSeed is a decentralised crowdfunding platform. Project creators launch campaigns and backers browse projects and contribute crypto to receive project tokens.`,
+    imgSrc: '/static/images/etherseed.png',
+    href: 'https://github.com/davidumoru/EtherSeed',
   },
   {
     title: 'FeedWell',
