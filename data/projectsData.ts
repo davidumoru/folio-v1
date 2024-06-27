68c37582b4ae2b95875a8eb9bf4ba86c63dcc644
David Umoru <umorudavido@gmail.com>
2024-06-27 20:37:14 +0100

    Update project data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -8,27 +8,27 @@ interface Project {
 const projectsData: Project[] = [
   {
     title: 'Scryer',
-    description: `Scryer unlocks valuable insights from web data using AI and web scraping techniques.`,
+    description: `(Work-In-Progress)--- Scryer unlocks valuable insights from web data using AI and web scraping techniques.`,
     imgSrc: '/static/images/scryer.png',
     href: 'https://github.com/davidumoru/scryer',
   },
   {
     title: 'Apex',
-    description: `Apex lets you deploy your frontend applications to the web with minimal effort.`,
+    description: `(Work-In-Progress)--- Apex lets you deploy your frontend applications to the web with minimal effort.`,
     imgSrc: '/static/images/apex.png',
-    href: 'https://github.com/davidumoru/EtherSeed',
+    href: 'https://github.com/davidumoru/',
   },
   {
-    title: 'EtherSeed',
-    description: `EtherSeed allows users launch project campaigns and crowdfunding using crypto.`,
-    imgSrc: '/static/images/etherseed.png',
-    href: 'https://github.com/davidumoru/EtherSeed',
+    title: 'Verifyr',
+    description: `Coming Soon`,
+    imgSrc: '',
+    href: 'https://github.com/davidumoru/',
   },
   {
     title: 'FeedWell',
     description: `Coming Soon`,
-    imgSrc: '/static/images/feedwell.png',
-    href: 'https://github.com/davidumoru/feedwell',
+    imgSrc: '',
+    href: 'https://github.com/davidumoru/',
   },
 ]
 
