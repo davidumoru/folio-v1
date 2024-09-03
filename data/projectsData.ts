ec425cc163dc3a7c9f7852621406d50e11fe144a
David Umoru <umorudavido@gmail.com>
2024-09-03 02:48:58 +0100

    Clear project data

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,56 +6,6 @@ interface Project {
   category: 'Fullstack' | 'Backend' | 'CLI'
 }
 
-const projectsData: Project[] = [
-  {
-    title: 'Scryer',
-    description: `(Work-In-Progress)--- Scryer unlocks valuable insights from web data using AI and web scraping techniques.`,
-    imgSrc: '/static/images/scryer.png',
-    href: 'https://github.com/davidumoru/scryer',
-    category: 'Fullstack',
-  },
-  {
-    title: 'Apex',
-    description: `(Work-In-Progress)--- Apex lets you deploy your frontend applications to the web with minimal effort.`,
-    imgSrc: '/static/images/apex.png',
-    href: 'https://github.com/davidumoru/',
-    category: 'Fullstack',
-  },
-  {
-    title: 'Verifyr',
-    description: `Coming Soon`,
-    imgSrc: '/static/images/verifyr.png',
-    href: 'https://github.com/davidumoru/verifyr',
-    category: 'Backend',
-  },
-  {
-    title: 'FeedWell',
-    description: `(Work-In-Progress)--- RSS feed aggregator that allows users to subscribe to multiple blogs and receive updates in one place.`,
-    imgSrc: '/static/images/feedwell.png',
-    href: 'https://github.com/davidumoru/feedwell',
-    category: 'Backend',
-  },
-  {
-    title: 'Git Helper',
-    description: `Command-line tool that simplifies common Git commands`,
-    imgSrc: '',
-    href: 'https://github.com/davidumoru/git-helper',
-    category: 'CLI',
-  },
-  {
-    title: 'Locksmith',
-    description: `Command-line tool for generating, managing, and securely encrypting passwords.`,
-    imgSrc: '',
-    href: 'https://github.com/davidumoru/locksmith',
-    category: 'CLI',
-  },
-  {
-    title: 'README Generator',
-    description: `Command-line tool that generates a well-structured README file for your projects.`,
-    imgSrc: '',
-    href: 'https://github.com/davidumoru/readme-generator',
-    category: 'CLI',
-  },
-]
+const projectsData: Project[] = []
 
 export default projectsData
